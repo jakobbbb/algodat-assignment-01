3692b94a51d185b76be28b570b72d6334eac78ff
Jakob Ruckel <jakob@ruckel.de>
2020-05-28 15:50:45 +0200

    add io functions

---
+++ source/io.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+template<typename T>
+void print(std::vector<T> const& xs) {
+  int n = xs.size();
+  for (int i = 0; i < n - 1; ++i) {
+    std::cout << xs[i] << ", ";
+  }
+  if (n == 0) {
+    std::cout << "(empty)";
+  } else {
+    std::cout << xs[n - 1];
+  }
+  std::cout << '\n';
+}
+
+template<typename T>
+std::vector<T> to_vector(int argc, char *argv[]) {
+  std::vector<T> xs;
+  for (int i = 1; i < argc; ++i) {
+    try {
+      xs.push_back((T) std::stod(argv[i]));
+    } catch (std::invalid_argument const& e) {
+      std::cerr << "Warning: ignoring invalid argument '" << argv[i] << "'.\n";
+    } catch (std::out_of_range const& e) {
+      std::cerr << "Warning: ignoring out-of-range '" << argv[i] << "'.\n";
+    }
+  }
+  return xs;
+}
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,8 +1,70 @@
 #define CATCH_CONFIG_MAIN
+#include "io.hpp"
 #include <catch2/catch.hpp>
+#include <array>
+#include <iostream>
 
-TEST_CASE("hello", "[hello]") {
-  SECTION("42 is 42", "[42]") {
-    REQUIRE(42 == 42);
+SCENARIO("print", "[print]") {
+  GIVEN("an empty vector of integers") {
+    std::vector<int> a{};
+    THEN("print it") {
+      CHECK_NOTHROW(print(a));
+    }
+  }
+  GIVEN("an non-empty vector of integers") {
+    std::vector<int> a{-1, 0, 1, 2, 3, 42, 1, 3, 4};
+    THEN("print it") {
+      CHECK_NOTHROW(print(a));
+    }
+  }
+  GIVEN("an non-empty vector of doubles") {
+    std::vector<double> a{-1.2, 0.5, 1.12, 2.534, 1.234, 42.2, 1.4, 3.61, 4.0};
+    THEN("print it") {
+      CHECK_NOTHROW(print(a));
+    }
+  }
+}
+
+SCENARIO("read input", "[read]") {
+  GIVEN("no input") {
+    int argc = 1;
+    char* argv[] = {(char*)""};
+    auto xs = to_vector<int>(argc, argv);
+    REQUIRE(xs.empty());
+  }
+  GIVEN("invalid input") {
+    int argc = 4;
+    char* argv[] = {(char*)"", (char*)"X", (char*)"Y", (char*)"Z"};
+    auto xs = to_vector<double>(argc, argv);
+    REQUIRE(xs.empty());
+  }
+  GIVEN("valid integer input") {
+    int argc = 5;
+    char* argv[] = {(char*)"", (char*)"-1", (char*)"0", (char*)"42", (char*)"1"};
+    auto xs = to_vector<int>(argc, argv);
+    REQUIRE(xs == std::vector<int>{-1, 0, 42, 1});
+  }
+  GIVEN("valid double input") {
+    int argc = 4;
+    char* argv[] = {(char*)"", (char*)"-1", (char*)"0.4", (char*)"-4.2"};
+    auto xs = to_vector<double>(argc, argv);
+    REQUIRE(xs[0] == Approx(-1));
+    REQUIRE(xs[1] == Approx(0.4));
+    REQUIRE(xs[2] == Approx(-4.2));
+    REQUIRE(xs.size() == 3);
+  }
+  GIVEN("partially valid integer input") {
+    int argc = 5;
+    char* argv[] = {(char*)"", (char*)"-1", (char*)"0", (char*)"X", (char*)"1"};
+    auto xs = to_vector<int>(argc, argv);
+    REQUIRE(xs == std::vector<int>{-1, 0, 1});
+  }
+  GIVEN("partially valid double input") {
+    int argc = 4;
+    char* argv[] = {(char*)"", (char*)"-1", (char*)"0.4", (char*)"X"};
+    auto xs = to_vector<double>(argc, argv);
+    REQUIRE(xs[0] == Approx(-1));
+    REQUIRE(xs[1] == Approx(0.4));
+    REQUIRE(xs.size() == 2);
   }
 }
